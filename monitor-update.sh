e7c2b3795ad9b019c00196bd1d1206e8050ce0f2
alberand <albershteyn.andrey@gmail.com>
2020-05-24 18:46:24 +0200

    fix: remove debug commands

--- monitor-update.sh
+++ monitor-update.sh
@@ -3,11 +3,9 @@
 case ${MONS_NUMBER} in
     1)
         mons -o
-        touch "${HOME}/hello-o"
         ;;
     2)
         mons -s
-        touch "${HOME}/hello-s"
         ;;
     *)
         # Handle it manually
